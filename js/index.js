36ca55fddba6f567c23900a8cbc93e1bfe66717a
Luiz Nunes <luiz.nunes@amplifique.me>
2022-06-23 14:43:35 -0300

    v2.0

--- js/index.js
+++ js/index.js
@@ -1,47 +1,30 @@
-var previousScrollPosition = window.pageYOffset;
-var nav = document.getElementById("nav");
-var menuIcon = document.getElementById("menu-icon");
-var navItems = document.getElementById("nav-items");
+const toggle = document.querySelectorAll("#language-toggle")[0];
+const pt = document.querySelectorAll(".pt");
+const en = document.querySelectorAll(".en");
+const buttons = document.querySelectorAll('.right-side-buttons')[0];
 
-// Show/Hide nav bar on scroll
-window.onscroll = function () {
-    var currentScrollPosition = window.pageYOffset;
-    if (previousScrollPosition > currentScrollPosition) {
-        nav.style.top = "0";
-    }
-    else {
-        nav.style.top = "-60px";
-    }
-    previousScrollPosition = currentScrollPosition;
-}
+buttons.addEventListener("click", () => {
+  if (toggle.checked == true) {
+    alert('Dude, this button is fake.');
+  } else {
+    alert('Cara, esse botão é falso.');
+  }
+});
 
-if (window.innerWidth <= 768) {
-    // Toggle nav menu button
-    menuIcon.addEventListener('click', function () {
-        navItems.classList.toggle("show");
-    })
-
-    // Toggle to close menu if user clicks a link or anywhere else
-    document.addEventListener("click", function (event) {
-        if ((navItems.classList.contains("show")) && (event.target != menuIcon)) {
-            navItems.classList.remove("show");
-        }
+toggle.addEventListener("change", () => {
+  if (toggle.checked == true) {
+    pt.forEach((x) => {
+      x.style.display = "none";
     });
-}
-
-// Always show nav items on wide screen
-if (window.innerWidth > 768) {
-    if (!navItems.classList.contains("show")) {
-        navItems.classList.add("show");
-    }
-}
-
-// Page load / reload to top of page
-if (history.scrollRestoration) {
-    history.scrollRestoration = 'manual';
-}
-else {
-    window.onbeforeunload = function () {
-        window.scrollTo(0, 0);
-    }
-}
+    en.forEach((x) => {
+      x.style.display = "block";
+    });
+  } else {
+    pt.forEach((x) => {
+      x.style.display = "block";
+    });
+    en.forEach((x) => {
+      x.style.display = "none";
+    });
+  }
+});
